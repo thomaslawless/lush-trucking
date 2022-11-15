6e4ef2a5ee2aad1c404c65becfb13244b4f59068
thomaslawless <lawlessptom@gmail.com>
2022-11-14 20:28:00 -0500

    nextJS to deploy on Varcel

---
+++ lush-trucking/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <h1>Lush Trucking</h1>
+      </Head>
+    </div>
+  )
+}
